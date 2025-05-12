fd9565ee9d36adcf9d1f16bba9d7486c1f784baf
hrmrakib <hrmrakibs@gmail.com>
2025-05-12 14:42:33 +0600

    checking primary diagonal matrix!

--- 2D-array/Checking_primary_diagonal_matrix.c
+++ 2D-array/Checking_primary_diagonal_matrix.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<stdbool.h>
 
 int main()
 {
@@ -16,15 +17,39 @@ int main()
         }
     }
 
-    printf("\n");
+    bool is_diagonal = true;
 
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++) {
-            if(i == j){
-                printf("i=>%d, j=%d\n", i, j);
-                printf("%d \n", a[i][j]);
+    if (row == col) // checking square matrix
+    {
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < col; j++)
+            {
+                if (i == j)
+                {
+                    // we are now in diagram
+                    // printf("i=>%d, j=%d\n", i, j);
+                    printf("%d \n", a[i][j]);
+                }
+                else
+                {
+                    // we are now outside of diagram
+                    if(a[i][j] != 0) {
+                        is_diagonal = false;
+                        printf("This is not a primary diagonal matrix!\n"); 
+                    }
+                }
             }
         }
+
+        if (is_diagonal == true)
+        {
+            printf("This is a primary diagonal matrix!\n"); 
+        }
+        
     }
-  
+    else
+    {
+        printf("This is not a primary diagonal matrix!\n");
+    }
 }
